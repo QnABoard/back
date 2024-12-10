5c753fd85c3cb332a086b41f693a5051eb2df24d
Dong Hyun <decidere@naver.com>
2024-12-10 20:36:30 +0900

    [Feat]: app.js 에러 핸들러 연결

--- src/app.js
+++ src/app.js
@@ -1,8 +1,10 @@
 import express from "express";
 import "dotenv/config.js";
+import errorHandler from "./middlewares/errorHandler.js";
 
 const app = express();
 app.use(express.json());
+app.use(errorHandler);
 
 const PORT_NUMBER = process.env.PORT_NUMBER;
 app.listen(PORT_NUMBER, () => {
