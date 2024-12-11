ed713d4d89ed2a66da4dbd1de6a0b4961fb61630
Dong Hyun <decidere@naver.com>
2024-12-11 16:31:52 +0900

    [Feat]: 닉네임 중복 확인 로직 구현

--- src/services/user.service.js
+++ src/services/user.service.js
@@ -11,3 +11,15 @@ export const checkEmailExists = async (email) => {
     throw new Error();
   }
 };
+
+// 닉네임 중복 확인
+export const checkNicknameExists = async (nickname) => {
+  try {
+    const sql = `SELECT 1 FROM users WHERE nickname = ?`;
+    const [exist] = await db.execute(sql, [nickname]);
+    if (exist[0]) return true;
+    else return false;
+  } catch (err) {
+    throw new Error();
+  }
+};
