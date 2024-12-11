3e842f5353f9271080eb4e595bf56b0fd80b859b
Dong Hyun <decidere@naver.com>
2024-12-11 18:22:02 +0900

    [Feat]: 비밀번호 암호화 로직 구현

--- src/services/user.service.js
+++ src/services/user.service.js
@@ -1,4 +1,6 @@
 import db from "../db/server.js";
+import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
 
 // 이메일 중복 확인
 export const checkEmailExists = async (email) => {
@@ -19,6 +21,16 @@ export const checkNicknameExists = async (nickname) => {
     const [exist] = await db.execute(sql, [nickname]);
     if (exist[0]) return true;
     else return false;
+  } catch (err) {
+    throw new Error();
+  }
+};
+
+// 비밀번호 암호화
+export const hashPassword = async (password) => {
+  try {
+    const salt = await bcrypt.genSalt(10);
+    return await bcrypt.hash(password, salt);
   } catch (err) {
     throw new Error();
   }
