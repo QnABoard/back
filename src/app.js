42713d3750e7f71fbe3ca992389dd83a00052c6d
Dong Hyun <decidere@naver.com>
2024-12-11 15:46:40 +0900

    [Feat]: app.js - user.routes 분리

--- src/app.js
+++ src/app.js
@@ -1,10 +1,13 @@
 import express from "express";
 import "dotenv/config.js";
 import errorHandler from "./middlewares/errorHandler.js";
+import userRouter from "./routes/user.routes.js";
 
 const app = express();
 app.use(express.json());
 app.use(errorHandler);
+
+app.use("/api/users", userRouter);
 
 const PORT_NUMBER = process.env.PORT_NUMBER;
 app.listen(PORT_NUMBER, () => {
