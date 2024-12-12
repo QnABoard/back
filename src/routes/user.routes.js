146d0823019842fa2944c081af603d8f24e59b29
Dong Hyun <decidere@naver.com>
2024-12-12 15:11:49 +0900

    [Fix]: 유저 API (회원가입, 로그인) 메서드 변경 GET -> POST

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -3,9 +3,9 @@ const router = express.Router();
 import userController from "../controllers/user.controller.js";
 
 // 회원가입
-router.get("/join", userController.registerUser);
+router.post("/join", userController.registerUser);
 
 // 로그인
-router.get("/login", userController.loginUser);
+router.post("/login", userController.loginUser);
 
 export default router;
