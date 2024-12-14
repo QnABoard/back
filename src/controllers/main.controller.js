9719261073195dba0d51cedd6116cee6daccbd25
Dong Hyun <decidere@naver.com>
2024-12-14 17:32:37 +0900

    [Feat]: 메인 화면 컨트롤러 응답데이터에 유저 정보 포함

--- src/controllers/main.controller.js
+++ src/controllers/main.controller.js
@@ -1,10 +1,26 @@
-import { getPostsData } from "../services/main.service.js";
+import {
+  getPostsData,
+  getTokenData,
+  getUserMainData,
+} from "../services/main.service.js";
 
 // 메인 화면
 const getMain = async (req, res, next) => {
+  const authHeader = req.headers.authorization;
   try {
-    const result = await getPostsData();
-    res.json(result);
+    let user;
+    // 토큰이 제공됐을 시 유저 데이터 조회
+    if (authHeader) {
+      const userTokenData = await getTokenData(authHeader);
+      user = await getUserMainData(userTokenData.email);
+    }
+
+    // 메인 화면 유저 데이터, 토큰 없을 시 null
+    user = user || null;
+
+    // 메인 화면 게시글 데이터
+    const posts = await getPostsData();
+    res.json({ user, posts });
   } catch (err) {
     next(err);
   }
