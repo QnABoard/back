f3969b0ee2d87e5c89fe7e5bc92e31ed7c974bc7
Dong Hyun <decidere@naver.com>
2024-12-14 22:50:08 +0900

    [Feat]: 게시글 조회 컨트롤러 응답데이터에 좋아요 스크랩 여부 추가

--- src/controllers/post.controller.js
+++ src/controllers/post.controller.js
@@ -1,12 +1,18 @@
 import {
   getPostDataById,
   getCommentDataById,
+  getLikeAndScrapStatus,
 } from "../services/post.service.js";
+
+import { getTokenData } from "../utils/getTokenData.js";
 
 // 게시글 조회
 const getPostById = async (req, res, next) => {
+  const authHeader = req.headers.authorization;
+  // 사용자의 스크랩, 좋아요 여부
   const { id } = req.params;
   try {
+    console.log(authHeader);
     const post = await getPostDataById(id);
     if (!post.length) {
       const error = new Error("게시글이 존재하지 않습니다.");
@@ -14,8 +20,12 @@ const getPostById = async (req, res, next) => {
       throw error;
     }
     const comments = await getCommentDataById(id);
+    if (!authHeader) return res.json({ post, comments });
 
-    res.json({ post, comments });
+    const user = await getTokenData(authHeader);
+    const status = await getLikeAndScrapStatus(user.email, id);
+
+    res.json({ post, status, comments });
   } catch (err) {
     next(err);
   }
