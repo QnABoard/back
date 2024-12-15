79d72f2ec3144758bb3b48d5690d7b5a3ffad651
Dong Hyun <decidere@naver.com>
2024-12-15 17:17:45 +0900

    [Comment]: 게시글 조회 컨트롤러 주석 추가

--- src/controllers/post.controller.js
+++ src/controllers/post.controller.js
@@ -9,21 +9,27 @@ import { getTokenData } from "../utils/getTokenData.js";
 // 게시글 조회
 const getPostById = async (req, res, next) => {
   const authHeader = req.headers.authorization;
-  // 사용자의 스크랩, 좋아요 여부
   const { id } = req.params;
   try {
+    // 게시글 데이터
     const post = await getPostDataById(id);
+    // 예외처리 404
     if (!post.length) {
       const error = new Error("게시글이 존재하지 않습니다.");
       error.statusCode = 404;
       throw error;
     }
+
+    // 댓글 데이터
     const comments = await getCommentDataById(id);
+
+    // 토큰 없을 시 게시글과 댓글만 전달
     if (!authHeader) return res.json({ post, comments });
 
     const user = await getTokenData(authHeader);
     const status = await getLikeAndScrapStatus(user.email, id);
 
+    // 토큰 있을 시 게시글, 좋아요&스크랩 여부, 댓글 전달
     res.json({ post, status, comments });
   } catch (err) {
     next(err);
