7245c84ba49f10cf9629c8164de69d5e8c19cd85
Dong Hyun <decidere@naver.com>
2024-12-16 21:53:46 +0900

    [Feat]: 유저가 작성한 게시물 조회 로직 구현

--- src/services/mypage.service.js
+++ src/services/mypage.service.js
@@ -1,9 +1,18 @@
 import db from "../db/server.js";
+import mainSQL from "../utils/mainSQL.js";
 
 // 프로필 정보 가져오기
 export const getProfile = async (nickname) => {
   const sql = `SELECT id, email, nickname, introduce, icon FROM users WHERE nickname = ?`;
 
   const [result] = await db.execute(sql, [nickname]);
+  return result[0];
+};
+
+// 유저가 작성한 게시물 조회
+export const getUserPosts = async (id) => {
+  const sql = mainSQL.base + ` WHERE p.user_id = ? ` + mainSQL.groupBy;
+
+  const [result] = await db.execute(sql, [id]);
   return result;
 };
