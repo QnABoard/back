ea9ed997de72dcf36c268fe8ec4a1715676f9cdb
Dong Hyun <decidere@naver.com>
2024-12-17 02:46:17 +0900

    [Feat]: 태그별 게시글 불러오기 컨트롤러 구현

--- src/controllers/main.controller.js
+++ src/controllers/main.controller.js
@@ -2,6 +2,7 @@ import {
   getPostsData,
   getUserMainData,
   getTags,
+  getPostByTag,
 } from "../services/main.service.js";
 
 import { getTokenData } from "../utils/getTokenData.js";
@@ -34,4 +35,20 @@ const getMain = async (req, res, next) => {
   }
 };
 
-export default { getMain };
+// 메인화면 태그별 게시글 조회
+const getMainPostsByTag = async (req, res, next) => {
+  const { tags, page = 1 } = req.query;
+  try {
+    // 태그별 게시물 id 조회
+    const postIdList = await getPostByTag(tags);
+
+    // 태그별 게시물 조회
+    const posts = await getPostsData(page, postIdList);
+
+    // 응답
+    res.status(200).json(posts);
+  } catch (err) {
+    next(err);
+  }
+};
+export default { getMain, getMainPostsByTag };
--- src/routes/main.routes.js
+++ src/routes/main.routes.js
@@ -2,6 +2,10 @@ import express from "express";
 const router = express.Router();
 import mainController from "../controllers/main.controller.js";
 
+// 메인 화면
 router.get("/", mainController.getMain);
+
+// 메인 화면: 태그별 게시물 불러오기
+router.get("/tags", mainController.getMainPostsByTag);
 
 export default router;
