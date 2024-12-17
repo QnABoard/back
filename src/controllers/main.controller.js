2c4d5ed10106f1b3b0cc78be834843e4c2c2ac73
Dong Hyun <decidere@naver.com>
2024-12-17 20:08:36 +0900

    [Feat]: 메인화면 페이지네이션 응답데이터 수정 1페이지 아닐 때는 게시글 정보만 보내주기

--- src/controllers/main.controller.js
+++ src/controllers/main.controller.js
@@ -8,11 +8,15 @@ import {
 const getMain = async (req, res, next) => {
   const { page = 1 } = req.query;
   try {
-    // 메인 화면 태그 데이터
-    const tags = await getTags();
-
     // 메인 화면 게시글 데이터
     const posts = await getPostsData(page);
+
+    if (page > 1) {
+      return res.status(200).json({ posts });
+    }
+
+    // 메인 화면 태그 데이터
+    const tags = await getTags();
 
     // 응답
     res.status(200).json({ tags, posts });
