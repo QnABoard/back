c469ab28760b4161bc4f36f6fab34e5a5add4898
Dong Hyun <decidere@naver.com>
2024-12-17 20:52:18 +0900

    [Feat]: 마이페이지 게시글 조회 관련 컨트롤러 수정 변경된 로직(getMypagePosts) 사용 및 페이지네이션을 쿼리로 page 전달받음

--- src/controllers/mypage.controller.js
+++ src/controllers/mypage.controller.js
@@ -1,13 +1,13 @@
 import {
   getProfile,
   getUserLikePostList,
-  getUserLikePosts,
-  getUserPosts,
+  getMypagePosts,
 } from "../services/mypage.service.js";
 import { checkNicknameExists } from "../services/user.service.js";
 
 // 마이페이지 데이터 조회
 const getMypage = async (req, res, next) => {
+  const { page = 1 } = req.query;
   const { nickname } = req.params;
   try {
     // 존재하지 않는 닉네임 예외처리 -> 닉네임 중복 체크 로직 재활용
@@ -22,7 +22,8 @@ const getMypage = async (req, res, next) => {
     const profile = await getProfile(nickname);
 
     // 유저 게시글
-    const posts = await getUserPosts(profile.id);
+    const where = ` WHERE p.user_id = ? `;
+    const posts = await getMypagePosts(profile.id, where, page);
 
     // 응답
     res.status(200).json({ profile, posts });
@@ -33,6 +34,7 @@ const getMypage = async (req, res, next) => {
 
 // 마이페이지 유저가 좋아요 한 게시글 조회
 const getMypageLikes = async (req, res, next) => {
+  const { page = 1 } = req.query;
   const { nickname } = req.params;
   try {
     // 존재하지 않는 닉네임 예외처리
@@ -47,7 +49,8 @@ const getMypageLikes = async (req, res, next) => {
     const list = await getUserLikePostList(nickname);
 
     // 좋아요한 게시글
-    const posts = await getUserLikePosts(list);
+    const where = " WHERE p.id IN (?)";
+    const posts = await getMypagePosts(list, where, page);
 
     // 응답
     res.status(200).json(posts);
