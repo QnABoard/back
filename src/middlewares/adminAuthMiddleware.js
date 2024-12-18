75d82bc2aa4046d42f0fe574a854a4d815b3f7b6
Dong Hyun <decidere@naver.com>
2024-12-19 02:00:09 +0900

    [Fix]: 멍청한짓 수정

--- src/middlewares/adminAuthMiddleware.js
+++ src/middlewares/adminAuthMiddleware.js
@@ -19,17 +19,15 @@ const adminAuthMiddleware = (req, res, next) => {
 
   const token = authHeader.split(" ")[1]; // Bearer <token>
 
-  try {
-    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+  const decoded = jwt.verify(token, process.env.JWT_SECRET);
 
-    if (decoded.role === "admin") {
-      next();
-    } else {
-      const error = new Error("권한이 없습니다.");
-      error.statusCode = 401;
-      throw error;
-    }
-  } catch (error) {}
+  if (decoded.role === "admin") {
+    next();
+  } else {
+    const error = new Error("권한이 없습니다.");
+    error.statusCode = 401;
+    throw error;
+  }
 };
 
 export default adminAuthMiddleware;
