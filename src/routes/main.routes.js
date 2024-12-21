67320e6617a63266473e64150733a57cd7d4b7a8
Dong Hyun <decidere@naver.com>
2024-12-21 16:08:20 +0900

    [Feat]: 게시글 검색 컨트롤러 구현

--- src/routes/main.routes.js
+++ src/routes/main.routes.js
@@ -8,4 +8,7 @@ router.get("/", mainController.getMain);
 // 메인 화면: 태그별 게시물 불러오기
 router.get("/tags", mainController.getMainPostsByTag);
 
+// 메인 화면: 검색
+router.get("/search", mainController.searchPost);
+
 export default router;
