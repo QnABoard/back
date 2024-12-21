5d0ea2979e93dacd97adbae57081f56e69ab375e
Dong Hyun <decidere@naver.com>
2024-12-21 16:35:12 +0900

    [Feat]: 댓글 등록 컨트롤러 구현

---
+++ src/controllers/comment.controller.js
@@ -0,0 +1,25 @@
+import { addCommentData } from "../services/comment.service.js";
+import ERRORS from "../utils/errors.js";
+
+// 댓글 등록
+const addComment = async (req, res, next) => {
+  const userId = req.user.id;
+  const postId = +req.params.id;
+  const { content } = req.body;
+
+  try {
+    // 빈 댓글 예외처리
+    if (!content) {
+      throw ERRORS.badRequest("내용을 입력해주세요");
+    }
+
+    // 댓글 등록
+    addCommentData(userId, postId, content);
+
+    res.json({ success: true });
+  } catch (err) {
+    next(err);
+  }
+};
+
+export default { addComment };
